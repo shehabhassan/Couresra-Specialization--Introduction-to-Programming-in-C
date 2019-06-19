7713820d898b4c3640cc6855c10ffaf21f6d413f
Coursera Learner <nobody@nowhere.nul>
2019-06-19 19:45:10 +0000

    assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -4,57 +4,56 @@
 //this program to calculate your retirement
 //struct the type of variable
 struct _retire_info {
-  int     month;
+  int     months;
   double contribution;
   double rate_of_return ;
 };
 typedef struct _retire_info retire_info ;
 
 //first function for calcluting  
-void retirement (int starAge , double initial , retire_info work , retire_info retired ){
+void retirement (int startAge , double initial , retire_info working , retire_info retired ){
   //variable definition
-   int age = starAge;
+   int age = startAge;
    int months= age%12;
    double balance =initial;
   //first loop for working
-   for(int i=0; i<work.month; i++)
+   for(int i=0; i<working.months; i++)
    {
      printf("Age %3d month %2d you have $%.2f\n" ,age,months,balance);
+    balance = balance +( balance * working.rate_of_return) + working.contribution ;
      months = months+1;
      if (months==12){
        age =age+1;
        months=0;}
-     balance = balance *(work.rate_of_return) + (work.contribution)+balance ;
    }
   //second loop for retired
-   for(int j=0 ; j<retired.month ;j++)
+   for(int j=0 ; j<retired.months ;j++)
    {
      printf("Age %3d month %2d you have $%.2f\n",age,months , balance);
+  balance = balance +(balance*retired.rate_of_return) + retired.contribution ; 
      months=months+1;
      if(months==12)
      {
        age = age+1;
        months=0;
      }
-     balance = balance*(retired.rate_of_return) - (retired.contribution)+balance ;
    }
-  
 }
 int main  ()
 {
   //fixed starting data
   int age =327/12;
   double balance =21345.0;
-  retire_info work ,retired ;
+  retire_info working ,retired ;
   // this given for work 
-  work.month = 489;
-  work.contribution = 1000;
-  work.rate_of_return = 0.045/12 ;
+  working.months = 489;
+  working.contribution = 1000;
+  working.rate_of_return = 0.045/12 ;
   //this given for retired
-  retired.month = 384 ;
+  retired.months = 384 ;
   retired.contribution = -4000;
   retired.rate_of_return= 0.01/12;
   //call function and running
-  retirement(age,balance,work,retired);
+  retirement(age,balance,working,retired);
   return EXIT_SUCCESS ; 
 }
