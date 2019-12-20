44d450df4b6875186da8dc8bcfae174f3d8d599e
Coursera Learner <nobody@nowhere.nul>
2019-12-20 20:47:30 +0000

    c2prj1

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,10 @@
 #include "cards.h"
 
 int main(void) {
-
+      assert_card_valid();
+      value_letter():
+      suit_letter():
+      ranking_to_string();
+ card_from_num():
+ card_from_letters():
 }
