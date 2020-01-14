32fc1c5cd49e1dae57ef14efcc03759bebcc6d37
Coursera Learner <nobody@nowhere.nul>
2020-01-14 20:51:09 +0000

    assignment 11

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -101,8 +101,8 @@ card_t card_from_letters(char value_let, char suit_let) {
 card_t card_from_num(unsigned c) {
   //card_from_letters();
   card_t temp;
-    temp.value = c%13 + 1;
-    temp.suit =c/13;
+  // temp.value = c%13 + 1;
+  //  temp.suit =c/13;
      if (temp.value>=2 && temp.value<=9){
        return  temp;//.value = c +'0';   
     }
