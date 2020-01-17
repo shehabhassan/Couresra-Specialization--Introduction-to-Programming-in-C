c74bc19654663e1ae485cb5ae6462861e8a58a2d
Coursera Learner <nobody@nowhere.nul>
2020-01-17 13:52:32 +0000

    assignment 11

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,13 +5,13 @@
 
 void assert_card_valid(card_t c) {
   // it is the function of check the correct value.
-    assert(c.value>=2);
-    assert(c.value<=VALUE_ACE);
-    assert(c.value!=0);
+    assert(c.value);
+    assert(VALUE_ACE);
+    assert(VALUE_KING);
   //it is the function of check the correct value.
-    assert(c.suit>=SPADES);
+    assert(SPADES);
     assert(c.suit!=0);
-    assert(c.suit<=NUM_SUITS);
+    assert(NUM_SUITS);
     // condition to check the correct value. 
   if(c.value<=2 || c.value>=VALUE_ACE){
     printf("error with value");
