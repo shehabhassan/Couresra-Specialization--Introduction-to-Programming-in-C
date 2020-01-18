f0c49632afd04ed159a5c66b66a792b96a1d9a16
Coursera Learner <nobody@nowhere.nul>
2020-01-18 17:07:08 +0000

    assignment 11

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,7 +5,7 @@
 
 void assert_card_valid(card_t c) {
   // it is the function of check the correct value.
-    assert(c.value >=1);
+    assert(c.value >=2);
     assert(c.value <= 14);
     //assert(c.value);
   //it is the function of check the correct value.
