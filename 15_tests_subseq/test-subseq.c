b671d20d0c5b9f01085837eb84206fd83d31f2a0
Coursera Learner <nobody@nowhere.nul>
2020-02-19 00:29:38 +0000

    assignment 15

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -6,41 +6,41 @@ size_t maxSeq(signed int *array , int n );
 int main (){
 
   int array1[] = {1,2,3,2};
-  if (maxSeq(array1,0)){
+  if (maxSeq(array1,4) != 3){
     printf("faild on 1,2,3,2 \n");
     exit(EXIT_FAILURE) ;
   }
-  if (maxSeq(array1,4) != 3){
-    printf("faild on 1,2,3,2\n ");
+  if (maxSeq(NULL,0)){
+    printf("faild on NULL\n ");
     exit(EXIT_FAILURE); 
   }
-  int array2[]={8,8,8,9};
-  if (maxSeq(array2,4) != 2){
-    printf("faild on 8,8,8,9\n");
+  int array2[]={-2};
+  if (maxSeq(array2,1) != 1){
+    printf("faild on -2\n");
     exit(EXIT_FAILURE);
   }
-  int array3[]={12};
-  if (maxSeq(array3,1)!= 1){
-    printf("faild on 12\n ");
+  int array3[]={8,8,8,8};
+  if (maxSeq(array3,4)!= 1){
+    printf("faild on 8,8,8,8\n ");
     exit(EXIT_FAILURE);
   }
-  int array4[] = {2,-3,5,6,8}; 
-  if (maxSeq(array4,5) != 4){
+  int array4[] = {12}; 
+  if (maxSeq(array4,1) != 1){
+    printf("faild on 12\n");
+    exit(EXIT_FAILURE);
+  }
+  int array5[] = {2,-3,5,6,8};
+  if (maxSeq(array5,5) != 4){
     printf("faild on 2,-3,5,6,8\n");
     exit(EXIT_FAILURE);
   }
-  int array5[] = {-15};
-  if (maxSeq(array5,1) != 1){
-    printf("faild on -15\n");
-    exit(EXIT_FAILURE);
-  }
   int array6[] ={2,4,3,6,4,10,15,-1,7,8,2};
-  if(maxSeq(array6,10) != 4){
+  if(maxSeq(array1,0)){
     printf("faild on 2,4,3,6,4,10,15,-1,7,8,2\n");
     exit(EXIT_FAILURE);
   }
-   if (maxSeq(NULL,0)){
-    printf("faild on NULL1\n");
+   if (maxSeq(array6,10) != 4){
+    printf("faild on \n");
     exit(EXIT_FAILURE);
   }
    
